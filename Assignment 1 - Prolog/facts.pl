18728e9a0da768c94d1f72b43c76e056e55885b5
Tanzia Ahmed <52648080+tanzia-ahmed@users.noreply.github.com>
2021-05-17 14:13:02 +0600

    rule-8 and naming conventions are modified for methods of java class

--- Assignment 1 - Prolog/facts.pl
+++ Assignment 1 - Prolog/facts.pl
@@ -17,7 +17,7 @@ defines(human,type,attribute,public).
 defines(human,empathy,attribute,public).
 defines(human,act,method,public).
 defines(human,reason,method,public).
-defines(human,hasempathy,method,public).
+defines(human,hasEmpathy,method,public).
 
 defines(bladerunner,type,attribute,public).
 defines(bladerunner,rank,attribute,public).
@@ -26,17 +26,22 @@ defines(bladerunner,reason,method,public).
 defines(machine,type,attribute,public).
 
 defines(android,version,attribute,public).
-defines(android,whatihave,method,public).
-defines(android,whatineed,method,public).
+defines(android,whatIhave,method,public).
+defines(android,whatIneed,method,public).
 defines(android,act,method,public).
 defines(android,reason,method,public).
 
-defines(android2,whatihave,method,public).
-defines(android2,hasempathy,method,public).
-defines(android2,hasmemories,method,public).
+defines(android2,whatIhave,method,public).
+defines(android2,hasEmpathy,method,public).
+defines(android2,hasMemories,method,public).
 
 defines(behavior2,empathy,attribute,public).
 defines(behavior2,memories,attribute,public).
+defines(behavior2,hasEmpathy,method,public).
+defines(behavior2,hasMemories,method,public).
+
+defines(behavior,act,method,public).
+defines(behavior,reason,method,public).
 
 extends(bladerunner,human).
 extends(android,machine).
@@ -92,6 +97,24 @@ findall(X,
            (extends(Type,Y),defines(Y,X,attribute,_));
            (implements(Type,Z),defines(Z,X,attribute,_))),
         State).
+
+% Rule 8
+interface_of(Type,Interface):-
+    findall(X,(defines(Type,X,method,_);
+               (   extends(Type,Y), defines(Y,X,method,_))),L1),
+    findall(X,(implements(Type,Y),defines(Y,X,method,_)),L2),
+    subset(L2,L1),
+    con(L1,L2,L3),
+    list_to_set(L3,Interface).
+% concat fn
+ con([],L1,L1).
+ con([X|Tail],L2,[X|Tail1]):- con(Tail,L2,Tail1).
+% list-equalTo fn
+%same([], []).
+%same([H1|R1], [H2|R2]):-
+   % H1 = H2,
+   % same(R1, R2).
+
 
 % Rule 9
 siblings(ListOfSiblings):-
